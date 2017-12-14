4c71f09e724c4115eb5c4de9f0303b9ea5b2f4ec
Landon Patmore <landon.patmore@gmail.com>
2017-12-14 18:51:21 -0500

    working on client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,38 @@
 package Client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Client {
+    private Socket socket;
+
+    public Client(){
+        openConnection();
+        try {
+            OutputStream output = socket.getOutputStream();
+            InputStream input = socket.getInputStream();
+
+            String message = new String(input.readAllBytes());
+
+            System.out.println(message);
+
+            output.close();
+            input.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openConnection(){
+        try {
+            socket = new Socket("nuc29.local", 9000);
+        } catch (IOException e) {
+            System.out.println("Cannot connect to host.");
+            System.exit(-1);
+        }
+    }
+
 }
